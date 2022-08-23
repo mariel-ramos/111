42e2f4497a45882b037327c370faebba85bb5036
Mariel A. Monte de Ramos <111344331+mariel-ramos@users.noreply.github.com>
2022-08-23 14:42:38 +0800

    Create App.java

---
+++ 111/App.java
@@ -0,0 +1,67 @@
+
+
+public class App {
+    public static void main(String[] args) {
+
+        public void sayMyName() {
+            System.out.println(Say my Name);
+
+            public void sayMyStrength() {
+
+                public void sayMyAgility() {
+
+                    public void SayMyIntelligence() {
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+
+        Character knight = new Character();
+
+    
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.agility();
+        knight.sayMyintelligence();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyagility();
+        mage.sayMyintelligence();
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyintelligence();
+
+        Character support = new Character();
+
+        support.strength = 6;
+        support.agility = 9;
+        support.intelligence = 0;
+        support.name = "kaja";
+        support.sayMyName();
+        support.sayMyStrength();
+        support.sayMyagility();
+        support.SayMyIntelligence();
+    }
+}
